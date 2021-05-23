2ae2f10dd4c17c80294ad309522c628c91ef179a
Rosellinda <64804606+Rosellinda@users.noreply.github.com>
2021-05-24 02:15:25 +0800

    Add complete task page

--- asset/script.js
+++ asset/script.js
@@ -3,21 +3,23 @@ window.onload = () => {
     let items = document.getElementById("items");
     let submit = document.getElementById("submit");
 
+
     let editItem = null;
 
     addForm.addEventListener("submit", addTask);
     items.addEventListener("click", removeItem);
+    items.addEventListener("click", completedItem);
 };
 
 let addTask = (e) => {
     e.preventDefault();
 
     if (submit.value != "Submit") {
-        console.log("Hello");
+        // console.log("Hello");
         editItem.target.parentNode.childNodes[0].data = document.getElementById("new_task").value;
         submit.value = "Submit";
         document.getElementById("new_task").value = "";
-        document.getElementById("lblsuccess").innerHTML= "Text edited successfully";
+        document.getElementById("lblsuccess").innerHTML= "Task edited successfully";
         document.getElementById("lblsuccess").style.display = "block";
         setTimeout(function() {
             document.getElementById("lblsuccess").style.display = "none";
@@ -25,15 +27,18 @@ let addTask = (e) => {
   
         return false;
     }
-
+    //from input
     let newTask = document.getElementById("new_task");
+    //The value from input
     let newItem = newTask.value;
+    //remove whitespace from both sides
     if (newItem.trim() == "" || newItem.trim() == null)
         return false;
     else document.getElementById("new_task").value = "";
     
+    //create a new tag
     let li = document.createElement("li");
-        li.className = "list-group-item";
+        li.className = "list-group-item listItem";
   
     let deleteButton = document.createElement("button");
         deleteButton.className = "btn-danger btn btn-sm m-1 delete";
@@ -51,29 +56,63 @@ let addTask = (e) => {
     items.appendChild(li);
 }
 
-function removeItem(e) {
+let removeItem = (e) => {
     e.preventDefault();
-    if (e.target.classList.contains("delete")) {
-        if (confirm("Are you Sure?")) {
-            let li = e.target.parentNode;
-            items.removeChild(li);
-            document.getElementById("lblsuccess").innerHTML= "Text deleted successfully";
-            document.getElementById("lblsuccess").style.display = "block";
-  
-            setTimeout(function() {
-                document.getElementById("lblsuccess")
-                        .style.display = "none";
-            }, 3000);
-        }
-    }
+
     if (e.target.classList.contains("edit")) {
         document.getElementById("new_task").value = e.target.parentNode.childNodes[0].data;
+        document.getElementById("lblsuccess").innerHTML= "Update your task!";
+        document.getElementById("lblsuccess").style.display = "block";
         submit.value = "EDIT";
         editItem = e;
     }
 }
 
-function toggleButton(ref, btnID) {
+let completedItem = (e) => {
+    e.preventDefault();
+
+    let completedTask = document.getElementById("completedItems");
+
+    if (e.target.classList.contains("delete")) {
+        if (confirm("Are you Sure?")) {
+            //li will become the parent
+            let li = e.target.parentNode;
+            //remove the first Button (editButton)
+            li.removeChild(li.childNodes[1]);
+            //remove the 2nd Button (deleteButton)
+            li.removeChild(li.childNodes[1]);
+            
+            let today = new Date();
+            let date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+            // let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            let time = today.toLocaleTimeString('PST');
+            // let dateTime = date;
+            let dateTime = date+' '+time;
+
+            console.log(li.childNodes[0]);
+            console.log(dateTime);
+
+            let newDate = document.createElement("p");
+                newDate.className = "date";
+                newDate.appendChild(document.createTextNode(dateTime));
+
+            li.appendChild(newDate);
+            completedTask.appendChild(li);
+            document.getElementById("lblsuccess").innerHTML= "Task was completed!";
+            document.getElementById("lblsuccess").style.display = "block";
+  
+            setTimeout(function() {
+                document.getElementById("lblsuccess").style.display = "none";
+            }, 3000);
+        }
+    }
+
+}
+
+let toggleButton = (ref, btnID) => {
+    // The disabled property sets or returns whether a text field is disabled, or not.
+    // true - The text field is disabled
+    // false - Default. The text field is not disabled
     document.getElementById(btnID).disabled = false;
 }
 
